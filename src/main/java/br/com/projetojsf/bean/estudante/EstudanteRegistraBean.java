5cc8fec7f71131bf9746a33390eaae390db8de15
Henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-06-29 19:49:16 -0300

    Estudado sobre metodos, CSS, Javascript, forEach

--- src/main/java/br/com/projetojsf/bean/estudante/EstudanteRegistraBean.java
+++ src/main/java/br/com/projetojsf/bean/estudante/EstudanteRegistraBean.java
@@ -4,10 +4,47 @@ import br.com.projetojsf.model.Estudante2;
 
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Named
 public class EstudanteRegistraBean implements Serializable {
     private Estudante2 estudante = new Estudante2();
+    private String[] nomes = {"Teste1","Teste2","Teste3","Teste4"};
+    private List<String> lista = new ArrayList<>();
+
+    public void executar(){
+        System.out.println("Executando um metodo!");
+    }
+
+    public String retorna(){
+        return "Retorna na tela " + getEstudante().getNome();
+    }
+    public String proximaPagina(){
+        return "index2?faces-redirect=true";
+    }
+
+    public List<String> getLista() {
+        lista.add("João");
+        lista.add("Maria");
+        lista.add("JOse");
+        lista.add("Antonio");
+        return lista;
+    }
+
+    public void setLista(List<String> lista) {
+        this.lista = lista;
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
 
     public Estudante2 getEstudante() {
         return estudante;
