61008c324e2322a3ce060dae81b7944098f5780b
Henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-06-30 22:30:53 -0300

    Exibindo dados com AJAX, manipulando dados e mostrando na tela

--- src/main/java/br/com/projetojsf/bean/estudante/EstudanteRegistraBean.java
+++ src/main/java/br/com/projetojsf/bean/estudante/EstudanteRegistraBean.java
@@ -2,6 +2,7 @@ package br.com.projetojsf.bean.estudante;
 
 import br.com.projetojsf.model.Estudante2;
 
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -10,10 +11,39 @@ import java.util.List;
 import java.util.Set;
 
 @Named
+@ViewScoped
 public class EstudanteRegistraBean implements Serializable {
     private Estudante2 estudante = new Estudante2();
     private String[] nomes = {"Teste1","Teste2","Teste3","Teste4"};
     private List<String> lista = new ArrayList<>();
+    private boolean mostrarNota;
+    private boolean mostrarLink;
+
+    public boolean isMostrarLink() {
+        return mostrarLink;
+    }
+
+    public void setMostrarLink(boolean mostrarLink) {
+        this.mostrarLink = mostrarLink;
+    }
+
+    public void exibirNota(){
+        this.mostrarNota = true;
+    }
+    public void exibirLink(){
+        this.mostrarLink = true;
+    }
+    public void esconderNota(){
+        this.mostrarNota = false;
+    }
+
+    public boolean isMostrarNota() {
+        return mostrarNota;
+    }
+
+    public void setMostrarNota(boolean mostrarNota) {
+        this.mostrarNota = mostrarNota;
+    }
 
     public void executar(){
         System.out.println("Executando um metodo!");
