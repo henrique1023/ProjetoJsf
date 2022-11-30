8e5f032a170b691531dac545a00ed13c31a2eea5
Henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-11-30 20:29:30 -0300

    54: Upload de Arquivos pt 02 Validações básicas

--- src/main/java/br/com/projetojsf/bean/validator/Validators.java
+++ src/main/java/br/com/projetojsf/bean/validator/Validators.java
@@ -6,6 +6,7 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
+import javax.servlet.http.Part;
 import java.io.Serializable;
 import java.util.List;
 
@@ -16,11 +17,45 @@ import static java.util.Arrays.asList;
 public class Validators implements Serializable {
     private List<String> emailsDB = asList("teste1@gmail.com", "teste2@gmail.com", "teste3@gmail.com");
 
-    public void validateDuplicatedEmail(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+    public void validateDuplicatedEmail(FacesContext facesContext, UIComponent uiComponent, Object o)
+            throws ValidatorException {
         String email = (String) o;
         if (emailsDB.contains(email)) {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "O email já existe", "");
             throw new ValidatorException(message);
+        }
+    }
+
+    public void validateFile(FacesContext facesContext, UIComponent uiComponent, Object o)
+            throws ValidatorException {
+        Part file = (Part) o;
+        try{
+            validateFileNameLength(file);
+            validateContentType(file);
+            validateFileSize(file);
+
+        }catch (RuntimeException e){
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    e.getMessage(), "");
+            throw new ValidatorException(message);
+        }
+    }
+
+    private void validateFileNameLength(Part file){
+        if(file.getSubmittedFileName().length() > 10){
+            throw new RuntimeException("O nome + a extensão não pode passar de 10 carac");
+        }
+    }
+
+    private void validateContentType(Part file){
+        String contentType = file.getContentType();
+        if(!contentType.equals("image/png") && !contentType.equals("image/jpge") ){
+            throw new RuntimeException("Apenas arquivos do tipo PNG ou JPGE aceitos!");
+        }
+    }
+    private void validateFileSize(Part file){
+        if(file.getSize() > 1048576){
+            throw new RuntimeException("Arquivo maior que 1Mb");
         }
     }
 }
